5e28bf94e8958f43752c4735dec5399a65ad509e
aston_laptop <astonbradleyjones@gmail.com>
2021-11-09 11:52:05 -0600

    Polished up google api endpoints.

--- routes/googleapi.js
+++ routes/googleapi.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const router = express.Router();
-
-const dbo = require('../db/conn');
+const dbo = require('../db/conn'); //DB connection
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 
 router.get('/', function(req, res){
     res.status(400).send("Bad Request. You do not have access.");
 })
 
-// Need to push the lead data to a DB and to VICI Dialer
 router.post('/leadData', function(req, res){
-const dbConnect = dbo.getDb().g_db; //get the google DB inside of Atlas Cluster
+if(req.body.google_key == GOOGLE_SECRET){
 
-dbConnect
-    .collection('google-leads')
-    .insertOne(req.body, function (err, result){
-        if(err) {
-            res.status(400).send('Error inserting data into Atlas Cluster');
-        } else {
-            console.log(`Added a new record into Atlas Cluster From Google`);
-            res.status(204).send();
-        }
+    //NEED TO PUSH DATA TO VICI DB HERE
+
+    const dbConnect = dbo.getDb().g_db; //get the google DB inside of Atlas Cluster
+
+    dbConnect
+        .collection('google-leads')
+        .insertOne(req.body, function (err, result){
+            if(err) {
+                res.status(400).send('Error inserting data into Atlas Cluster');
+            } else {
+                console.log(`Added a new record into Atlas Cluster From Google`);
+                res.status(204).send();
+            }
     });
+} else {
+    res.status(400).send("Incorrect Authorization");
+}
 
-    console.log(`Google lead data: ${req.body}`);
 });
 
 module.exports = router;
