693ea2dd743101f48702b70fb07223b249a4c792
Ubuntu <ubuntu@ip-172-31-34-51.us-east-2.compute.internal>
2021-11-11 23:17:11 +0000

    attempted to change axios to https from aws ec2 instance.

--- routes/googleapi.js
+++ routes/googleapi.js
@@ -5,21 +5,22 @@ const dbo = require('../db/conn'); //DB connection
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 const VICI_USER = process.env.VICI_USER;
 const VICI_PASS = process.env.VICI_PASS;
+const VICI_IP = process.env.VICI_IP;
 
 router.get('/', function(req, res){
     res.status(400).send("Bad Request. You do not have access.");
 })
 
-router.post('/leadData', function(req, res){
+router.post('/leadData', async function(req, res){
 if(req.body.google_key == GOOGLE_SECRET){
     //url parameters to send with post request
-    const leadData  = {
+    const params = {
         phone_code: "1",
         list_id: "98769876", //ASSIGNED TO TEST LIST
         source: "This lead is coming from google ads.",
         function: "add_lead",
-        user: VICI_USER,
-        pass: VICI_PASS,
+        user: "5004",
+        pass: "agent5004",
         first_name: "",
         last_name: "",
         phone_number: ""
@@ -31,22 +32,27 @@ if(req.body.google_key == GOOGLE_SECRET){
         switch (element.column_id){
             case "FULL_NAME":
                 var nameArr = element.string_value.split(' ');
-                leadData.first_name = nameArr[0];
-                leadData.last_name = nameArr[1];
-                console.log(`First Name: ${leadData.first_name} - Last Name: ${leadData.last_name}`);
+                params.first_name = nameArr[0];
+                params.last_name = nameArr[1];
+		console.log("first name added")
                 break;
             case "PHONE_NUMBER":
-                leadData.phone_number = element.string_value;
-                console.log(`The Phone number is: ${leadData.phone_number}`);
-                break;
+                params.phone_number = element.string_value;
+                console.log("phone number Added");
+		break;
             default:
                 console.log(`ATTEMPTED TO IMPORT MISSING VALUE FROM GOOGLE LEAD. VALUE: ${element.column_id} DID NOT PROCESS CORRECTLY`);
         }
     });
 
     //Push to Vici Dialer
-    try { lead = await axios.post(`http://${VICI_IP}/vicidial/non_agent_api.php?`, leadData) }
-    catch (err) { console.log(`AXIOS RESPONSE ERROR ==> ${err}`) }
+    await axios({
+	method: 'post',
+	url: 'http://12.184.68.100/vicidial/non_agent_api.php',
+	data: params,
+    })
+    .then("axios request went through")
+    .catch("Error Pushing to Vici")
 
     const dbConnect = dbo.getDb().g_db; //get the google DB inside of Atlas Cluster
 
